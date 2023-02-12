faf4ef20bef480c7c6cefefa6f39b1209dcdc556
Jackbutler1991 <djackbutler1991@gmail.com>
2023-02-12 18:10:02 +0300

    corrected Home Work3

--- HW3.py
+++ HW3.py
@@ -1,32 +1,32 @@
 import random
 
-# Задание 1.2
-a = 1
-a1 = 1.1
-b = 'asd'
-lst = [1, 2, 3, 4]
-d = {'dasfd': 1, 'ghjk': 2}
-bo = True
+# Задание 1.2 из Практики лекции 3
+var_a = 1
+var_a1 = 1.1
+var_b = 'asd'
+lst1 = [1, 2, 3, 4]
+d_1 = {'dasfd': 1, 'ghjk': 2}
+var_bool = True
 set_1 = (1, 2, 3)
-print(type(a))
-print(type(a1))
-print(type(b))
-print(type(lst))
-print(type(d))
-print(type(bool))
+print(type(var_a ))
+print(type(var_a1))
+print(type(var_b))
+print(type(lst1))
+print(type(d_1))
+print(type(var_bool))
 print(type(set_1))
 
 # Задание 1.3 Найти значение выражений
-x = 17/2 * 3 + 2
-print (x)
-x = 2 + 17 / 2 * 3
-print (x)
-x = 19 % 4 + 15 / 2 * 3
-print (x)
-x = (15 + 6) - 10 * 4
-print (x)
-x = 17 / 2 % 2 * 3 ** 3
-print (x)
+x1 = ((17/2) * 3) + 2
+print (x1)
+x2 = 2 + ((17 / 2) * 3)
+print (x2)
+x3 = (19 % 4) + ((15 / 2) * 3)
+print (x3)
+x4 = (15 + 6) - (10 * 4)
+print (x4)
+x5 = ((17 / 2) % 2) * (3 ** 3)
+print (x5)
 
 # Задание 1.4 Создать три переменные, содержащие возраст трех...
 neigh1, neigh2, neigh3 = 19, 23,  39
@@ -41,24 +41,24 @@ int_namber2 = int(2.99)
 print(int_namber1, int_namber2)
 
 # WM 3.2 Заменить символ # в строке
-a = 'www.my_site.com#about'
-a = a.replace("#", "/")
-print(a)
+var_change = 'www.my_site.com#about'
+var_change = var_change.replace("#", "/")
+print(var_change)
 
 # HW 3.3 Добавить 'ing' к слову stroka
-a = "stroka"
-a = "stroka" + 'ing'
-print(a)
+var_ing_add = "stroka"
+var_ing_add = "stroka" + 'ing'
+print(var_ing_add)
 
 # HW 3.4 в строке Ivan Ivanov поменять слова местами
 str_ivan = "Ivan Ivanov"
-s = ' '.join(str_ivan.split(' ')[::-1])
-print(s)
+str_ivanov = ' '.join(str_ivan.split(' ')[::-1])
+print(str_ivanov)
 
 # HW 3.5 удалить пробелы вначале и вконце строки
-b = " HELLO "
-b = a.strip()
-print(b)
+var_del = " HELLO "
+var_del = var_del.strip()
+print(var_del)
 
 # HW 3.6 создайте переменную School
 school = {"1а": 23, "1б": 22, "1в": 21, "2a": 20, "2,б": 19, "2в": 20, "3а": 18, "4б": 24, "5а": 23, "6а": 15, }
