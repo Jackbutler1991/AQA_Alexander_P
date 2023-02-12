44c0491571366e4173963f6a585e414175666ef0
Jackbutler1991 <djackbutler1991@gmail.com>
2023-02-12 18:16:58 +0300

    corrected(reformat code) Home Work3

--- HW3.py
+++ HW3.py
@@ -8,7 +8,7 @@ lst1 = [1, 2, 3, 4]
 d_1 = {'dasfd': 1, 'ghjk': 2}
 var_bool = True
 set_1 = (1, 2, 3)
-print(type(var_a ))
+print(type(var_a))
 print(type(var_a1))
 print(type(var_b))
 print(type(lst1))
@@ -17,20 +17,20 @@ print(type(var_bool))
 print(type(set_1))
 
 # Задание 1.3 Найти значение выражений
-x1 = ((17/2) * 3) + 2
-print (x1)
+x1 = ((17 / 2) * 3) + 2
+print(x1)
 x2 = 2 + ((17 / 2) * 3)
-print (x2)
+print(x2)
 x3 = (19 % 4) + ((15 / 2) * 3)
-print (x3)
+print(x3)
 x4 = (15 + 6) - (10 * 4)
-print (x4)
+print(x4)
 x5 = ((17 / 2) % 2) * (3 ** 3)
-print (x5)
+print(x5)
 
 # Задание 1.4 Создать три переменные, содержащие возраст трех...
-neigh1, neigh2, neigh3 = 19, 23,  39
-sum_old = neigh1+ neigh2 + neigh3
+neigh1, neigh2, neigh3 = 19, 23, 39
+sum_old = neigh1 + neigh2 + neigh3
 print(sum_old)
 avr_old = (neigh1 + neigh2 + neigh3) / 3
 print(avr_old)
@@ -61,7 +61,8 @@ var_del = var_del.strip()
 print(var_del)
 
 # HW 3.6 создайте переменную School
-school = {"1а": 23, "1б": 22, "1в": 21, "2a": 20, "2,б": 19, "2в": 20, "3а": 18, "4б": 24, "5а": 23, "6а": 15, }
+school = {"1а": 23, "1б": 22, "1в": 21, "2a": 20, "2,б": 19, "2в": 20,
+          "3а": 18, "4б": 24, "5а": 23, "6а": 15, }
 
 # HW 3.7 cоздайте список и извлеките из него списка второй элемент
 hw_list = [1, 123.1, 'Petr', [1, 3.14], 'Minsk']
@@ -78,4 +79,4 @@ print(str_output[1])
 print(str_output[3:16:3])
 
 # Напишите программу, которая будет выводить уникальное число
-print(random.randrange(1, 100))  # выводит случайное число в промежутке от 1 до 100
+print(random.randrange(1, 100))  # случайное число в промежутке от 1 до 100
