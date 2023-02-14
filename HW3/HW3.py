fb3d632238175d8d0ae9137e92e506152f204c8a
Jackbutler1991 <djackbutler1991@gmail.com>
2023-02-14 18:01:32 +0300

    edit Home Work3

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -1,40 +1,5 @@
 import random
-from HW3_Package import *
-
-# Задание 1.2 из Практики лекции 3
-var_a = 1
-var_a1 = 1.1
-var_b = 'asd'
-lst1 = [1, 2, 3, 4]
-d_1 = {'dasfd': 1, 'ghjk': 2}
-var_bool = True
-set_1 = (1, 2, 3)
-print(type(var_a))
-print(type(var_a1))
-print(type(var_b))
-print(type(lst1))
-print(type(d_1))
-print(type(var_bool))
-print(type(set_1))
-
-# Задание 1.3 Найти значение выражений
-x1 = ((17 / 2) * 3) + 2
-print(x1)
-x2 = 2 + ((17 / 2) * 3)
-print(x2)
-x3 = (19 % 4) + ((15 / 2) * 3)
-print(x3)
-x4 = (15 + 6) - (10 * 4)
-print(x4)
-x5 = ((17 / 2) % 2) * (3 ** 3)
-print(x5)
-
-# Задание 1.4 Создать три переменные, содержащие возраст трех...
-neigh1, neigh2, neigh3 = 19, 23, 39
-sum_old = neigh1 + neigh2 + neigh3
-print(sum_old)
-avr_old = (neigh1 + neigh2 + neigh3) / 3
-print(avr_old)
+from __init__ import swap
 
 # HW 3.1 Привести к целому типу -1.6, 2.99
 int_namber1 = int(-1.6)
@@ -70,7 +35,7 @@ school = {"1а": 23, "1б": 22, "1в": 21, "2a": 20, "2,б": 19, "2в": 20,
 
 # HW 3.7 cоздайте список и извлеките из него списка второй элемент
 hw_list = [1, 123.1, 'Petr', [1, 3.14], 'Minsk']
-print(hw_list[3])
+print(hw_list[1])
 
 # HW 3.8 Вывести входит ли строка1 в строку2 (пример: employ и employment )
 str_find = "Houston, we have a problem"
